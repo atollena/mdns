0c10749c6d26f4c99b6f73e76e30bd931f4e0e65
Antoine Tollenaere <atollena@gmail.com>
2024-11-06 16:16:15 +0100

    reproduce-popup

--- go.mod
+++ go.mod
@@ -1,8 +1,17 @@
 module github.com/hashicorp/mdns
 
-go 1.13
+go 1.22.0
+
+toolchain go1.23.2
 
 require (
-	github.com/miekg/dns v1.1.55
-	golang.org/x/net v0.19.0
+	github.com/miekg/dns v1.1.62
+	golang.org/x/net v0.30.0
+)
+
+require (
+	golang.org/x/mod v0.21.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/tools v0.26.0 // indirect
 )
--- server_test.go
+++ server_test.go
@@ -5,6 +5,7 @@ package mdns
 
 import (
 	"fmt"
+	"golang.org/x/net/nettest"
 	"testing"
 	"time"
 )
@@ -62,6 +63,67 @@ func TestServer_Lookup(t *testing.T) {
 		Timeout:     50 * time.Millisecond,
 		Entries:     entries,
 		DisableIPv6: true,
+	}
+	err = Query(params)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	err = <-errCh
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+}
+
+func TestServer_Loopback(t *testing.T) {
+	lo, err := nettest.LoopbackInterface()
+	if err != nil {
+		t.Fatalf("could not get loopback interface: %v", err)
+	}
+	serv, err := NewServer(&Config{
+		Zone:  makeServiceWithServiceName(t, "_foobar._tcp"),
+		Iface: lo,
+	})
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	defer func() {
+		if err := serv.Shutdown(); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+	}()
+
+	entries := make(chan *ServiceEntry, 1)
+	errCh := make(chan error, 1)
+	defer close(errCh)
+	go func() {
+		select {
+		case e := <-entries:
+			if e.Name != "hostname._foobar._tcp.local." {
+				errCh <- fmt.Errorf("Entry has the wrong name: %+v", e)
+				return
+			}
+			if e.Port != 80 {
+				errCh <- fmt.Errorf("Entry has the wrong port: %+v", e)
+				return
+			}
+			if e.Info != "Local web server" {
+				errCh <- fmt.Errorf("Entry as the wrong Info: %+v", e)
+				return
+			}
+			errCh <- nil
+		case <-time.After(80 * time.Millisecond):
+			errCh <- fmt.Errorf("Timed out waiting for response")
+		}
+	}()
+
+	params := &QueryParam{
+		Service:     "_foobar._tcp",
+		Domain:      "local",
+		Timeout:     50 * time.Millisecond,
+		Entries:     entries,
+		DisableIPv6: true,
+		Interface:   lo,
 	}
 	err = Query(params)
 	if err != nil {
